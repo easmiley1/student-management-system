5634ef36437f212f84268e0e8619ccd01d1e57f8
S.Naren.Kumar@gmail.com <S.Naren.Kumar@gmail.com@94c80147-2afa-fe43-e56c-0e8028ffb3ee>
2012-12-23 05:07:22 +0000

    Modified to retrieveAllStudents

--- trunk/SMSService/src/java/com/ish/sms/service/ops/AssociateOperations.java
+++ trunk/SMSService/src/java/com/ish/sms/service/ops/AssociateOperations.java
@@ -1,24 +1,60 @@
 package com.ish.sms.service.ops;
 
+import java.util.List;
+
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ish.sms.service.dao.AssociateOperationsDAO;
 import com.ish.sms.service.dto.StudentDTO;
+import com.ish.sms.service.dto.StudentList;
 import com.ish.sms.service.entity.Student;
 
-public class AssociateOperations extends EntityOperations{
+/**
+ * Class to handle all business logic for associate related entity database operations
+ * 
+ * @author Naren
+ *
+ */
+public class AssociateOperations extends EntityOperations {
 
 	@Autowired
 	private AssociateOperationsDAO associateOperationsDAO;
-	
+
+	/**
+	 * Method to create or update a student.
+	 * 
+	 * @param studentDTO
+	 * @return studentDTO
+	 * @throws Exception
+	 */
 	@Transactional
-	public StudentDTO createOrUpdateStudent(StudentDTO studentDTO) throws Exception{
+	public StudentDTO createOrUpdateStudent(StudentDTO studentDTO) throws Exception {
+
 		Student student = new Student();
 		BeanUtils.copyProperties(student, studentDTO);
 		student = associateOperationsDAO.createOrUpdateStudent(student);
 		BeanUtils.copyProperties(studentDTO, student);
 		return studentDTO;
+	}
+
+	/**
+	 * Method to return the list of all the students in the school.
+	 * 
+	 * @return studentList
+	 * @throws Exception
+	 */
+	@Transactional(readOnly=true)
+	public StudentList retrieveAllStudents() throws Exception {
+
+		StudentList studentList = new StudentList();
+		List<Student> studentsList = associateOperationsDAO.retrieveAllStudents();
+		for (Student student : studentsList) {
+			StudentDTO studentDTO = new StudentDTO();
+			BeanUtils.copyProperties(studentDTO, student);
+			studentList.getStudentDTOList().add(studentDTO);
+		}
+		return studentList;
 	}
 }
---
+++ trunk/SMSService/src/java/com/ish/sms/service/util/QueryConstants.java
@@ -0,0 +1,14 @@
+package com.ish.sms.service.util;
+
+/**
+ * 
+ * This interface has all the query name related constants
+ * 
+ * @author Naren 
+ * 
+ */
+public interface QueryConstants {
+
+	public static final String FIND_ALL_STUDENTS = "FIND_ALL_STUDENTS";
+	public static final String FIND_ALL_STUDENTS_QUERY = "select st from com.ish.sms.service.entity.Student st";
+}
