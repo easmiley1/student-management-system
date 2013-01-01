193a70e4da5a5a6b616489ba33656a024594b39f
S.Naren.Kumar@gmail.com <S.Naren.Kumar@gmail.com@94c80147-2afa-fe43-e56c-0e8028ffb3ee>
2013-01-01 04:36:02 +0000

    New Teacher pages

--- SMSService/src/java/com/ish/sms/service/dao/AssociateOperationsDAO.java
+++ SMSService/src/java/com/ish/sms/service/dao/AssociateOperationsDAO.java
@@ -3,6 +3,7 @@ package com.ish.sms.service.dao;
 import java.util.List;
 
 import com.ish.sms.service.entity.Student;
+import com.ish.sms.service.entity.Teacher;
 
 /**
  * DAO class for all the associate related entities.
@@ -23,6 +24,15 @@ public class AssociateOperationsDAO extends BaseOperationsDAO {
 	}
 
 	/**
+	 * Method to create or update a teacher.
+	 * 
+	 * @param teacher
+	 * @return teacher
+	 */
+	public Teacher createOrUpdateTeacher(Teacher teacher) {
+		return entityManager.merge(teacher);
+	}
+	/**
 	 * Method to return the list of all the students in the school.
 	 * 
 	 * @return studentList
@@ -33,4 +43,17 @@ public class AssociateOperationsDAO extends BaseOperationsDAO {
 		List<Student> studentsList = (List<Student>) entityManager.createNamedQuery(FIND_ALL_STUDENTS).getResultList();
 		return studentsList;
 	}
+
+	/**
+	 * Method to return the list of all the teachers in the school.
+	 * 
+	 * @return teacherList
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Teacher> retrieveAllTeachers() {
+
+		List<Teacher> teachersList = (List<Teacher>) entityManager.createNamedQuery(FIND_ALL_TEACHERS).getResultList();
+		return teachersList;
+	}
+	
 }
--- SMSService/src/java/com/ish/sms/service/rest/SMSAssociateService.java
+++ SMSService/src/java/com/ish/sms/service/rest/SMSAssociateService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import com.ish.sms.service.dto.StudentDTO;
 import com.ish.sms.service.dto.StudentListDTO;
+import com.ish.sms.service.dto.TeacherDTO;
+import com.ish.sms.service.dto.TeacherListDTO;
 
 /**
  * Restful service class to handle all Associate related operations.
@@ -45,6 +47,29 @@ public class SMSAssociateService extends SMSBaseService{
 	}
 
 	/**
+	 * Method to create or update a teacher and return the persisted teacherXML.
+	 * 
+	 * @param teacherXml
+	 * @return teacherXml
+	 */
+	@POST
+	@Path("/createTeacher/")
+	@Produces("text/xml")
+	@Consumes("text/xml")
+	public String createTeacher(String xml) {
+
+		TeacherDTO teacherDTO = null;
+		String teacherXml = null;
+		try {
+			teacherDTO = serviceTransformer.tryParseXml(xml);
+			teacherDTO = associateOperations.createOrUpdateTeacher(teacherDTO);
+			teacherXml = serviceTransformer.transformToXML(teacherDTO, "teacherDTO");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return teacherXml;
+	}	
+	/**
 	 * Method to return the list of all the students in the school.
 	 * 
 	 * @return studentListXML
@@ -64,4 +89,24 @@ public class SMSAssociateService extends SMSBaseService{
 		return studentListXML;
 	}
 
+	/**
+	 * Method to return the list of all the teachers in the school.
+	 * 
+	 * @return teacherListXML
+	 */
+	@POST
+	@Path("/retrieveAllTeachers/")
+	@Produces("text/xml")
+	public String retrieveAllTeachers() {
+
+		String teacherListXML = null;
+		try {
+			TeacherListDTO teacherListDTO = associateOperations.retrieveAllTeachers();
+			teacherListXML = serviceTransformer.transformToXML(teacherListDTO, "teacherListDTO");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return teacherListXML;
+	}
+	
 }
