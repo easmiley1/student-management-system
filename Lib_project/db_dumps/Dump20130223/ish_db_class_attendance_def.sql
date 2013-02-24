a83ecbbb24baa799b30534a53ea135aa0f09f778
S.Naren.Kumar@gmail.com <S.Naren.Kumar@gmail.com@94c80147-2afa-fe43-e56c-0e8028ffb3ee>
2013-02-24 03:28:53 +0000

    databse dump - 02/23/2013

---
+++ Lib_project/db_dumps/Dump20130223/ish_db_class_attendance_def.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `ish_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ish_db`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: ish_db
+-- ------------------------------------------------------
+-- Server version	5.5.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `class_attendance_def`
+--
+
+DROP TABLE IF EXISTS `class_attendance_def`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `class_attendance_def` (
+  `attendance_month_id` int(11) NOT NULL AUTO_INCREMENT,
+  `month_year` varchar(30) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  PRIMARY KEY (`attendance_month_id`),
+  UNIQUE KEY `attendance_month_id_UNIQUE` (`attendance_month_id`),
+  KEY `class_id_ref_idx` (`class_id`),
+  CONSTRAINT `class_id_ref` FOREIGN KEY (`class_id`) REFERENCES `class_details` (`class_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8 COMMENT='table to contain all the months for attendance register for a particular class';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class_attendance_def`
+--
+
+LOCK TABLES `class_attendance_def` WRITE;
+/*!40000 ALTER TABLE `class_attendance_def` DISABLE KEYS */;
+INSERT INTO `class_attendance_def` VALUES (50,'March-2013',1),(52,'January-2013',1),(54,'August-2013',1);
+/*!40000 ALTER TABLE `class_attendance_def` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-02-23 22:27:42
---
+++ Lib_project/db_dumps/Dump20130223/ish_db_class_details.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE  IF NOT EXISTS `ish_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ish_db`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: ish_db
+-- ------------------------------------------------------
+-- Server version	5.5.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `class_details`
+--
+
+DROP TABLE IF EXISTS `class_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `class_details` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(5) NOT NULL,
+  `class_teacher` int(11) NOT NULL,
+  `student_count` varchar(45) DEFAULT NULL,
+  `class_start` int(11) NOT NULL,
+  `class_end` int(11) NOT NULL,
+  `active` varchar(45) NOT NULL,
+  PRIMARY KEY (`class_id`),
+  UNIQUE KEY `class_id_UNIQUE` (`class_id`),
+  UNIQUE KEY `class_name_UNIQUE` (`class_name`),
+  KEY `teacher_ref_idx` (`class_teacher`),
+  CONSTRAINT `teacher_ref` FOREIGN KEY (`class_teacher`) REFERENCES `teacher_details` (`teacher_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='table to represent each class';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class_details`
+--
+
+LOCK TABLES `class_details` WRITE;
+/*!40000 ALTER TABLE `class_details` DISABLE KEYS */;
+INSERT INTO `class_details` VALUES (1,'LKG A',1,'1',2013,2014,'active'),(2,'LKG B',2,'2',2013,2014,'active'),(3,'UKG C',3,'10',2013,2014,'acive');
+/*!40000 ALTER TABLE `class_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-02-23 22:27:46
